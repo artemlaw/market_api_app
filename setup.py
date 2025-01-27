ae7977616fd56708a80147172c5aacdf8287aaf8
Artem Lubentsov <artem.law@mail.ru>
2025-01-27 11:20:26 +0300

    Корректировка временной заглушки по закрывающимся категориям Ya

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='market_api_app',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=['requests', 'pandas', 'openpyxl'],
     extras_require={
